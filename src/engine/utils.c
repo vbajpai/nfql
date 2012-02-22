7fa5072b5dbfe60418b28eceaf4b9b79ca4dad05
Vaibhav Bajpai <contact@vaibhavbajpai.com>
2012-02-22 16:07:34 +0100

    removed Makefile, removed {treesearch}main.c, removed filter.c, added utils.c

---
+++ src/engine/utils.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2011 Johannes 'josch' Schauer <j.schauer@email.de>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+
+/*
+ * adapted from ./stdlib/bsearch.c from the GNU C Library
+ * pass additional argument thunk to compar to allow it access additional data
+ * without global variables - in our case, we need to pass the data offset
+ */
+void *bsearch_r(const void *key, const void *base, size_t nmemb, size_t size,
+        int (*compar) (const void *, const void *, void *thunk), void *thunk)
+{
+    size_t l, u, idx;
+    const void *p;
+    int comparison;
+
+    l = 0;
+    u = nmemb;
+    while (l < u) {
+        idx = (l + u) / 2;
+        p = (void *) (((const char *) base) + (idx * size));
+        comparison = (*compar) (key, p, thunk);
+        if (comparison < 0) {
+            u = idx;
+        } else if (comparison > 0) {
+            l = idx + 1;
+        } else {
+            return (void *) p;
+        }
+    }
+    return NULL;
+}
