f2c9d0ab891d0008abb143b893e54f481401b10d
Nikolay Melnikov <nmelnikov@crystal.eecs.jacobs-university.de>
2012-07-20 12:30:57 +0200

    working filter stage scripts

--- engine/benchmarks/run.sh
+++ engine/benchmarks/run.sh
@@ -1,14 +1,14 @@
 # run nfdump
-sudo python nfdump.py /home/vbajpai/traces/nf/ july/queries/nfdump/filter/
+# sudo python nfdump.py /home/nmelnikov/traces/nf/ july/queries/nfdump/filter/
 
 # run flowtools
-sudo python flowtools.py /home/vbajpai/traces/ft/ july/queries/flowtools/filter/
+# sudo python flowtools.py /home/nmelnikov/traces/ft/ july/queries/flowtools/filter/
 
 # run silk
-sudo python silk.py /home/vbajpai/traces/silk/ july/queries/silk/filter/
+# sudo python silk.py /home/nmelnikov/traces/silk/ july/queries/silk/filter/
 
-# run nfql
-sudo python nfql.py bin/engine /home/vbajpai/traces/ft/ july/queries/nfql/filter/
+# run nfql-engine
+# sudo python nfql-engine.py ../bin/engine /home/nmelnikov/traces/ft/ july/queries/nfql/filter/
 
 # run nfql-filter
-sudo python nfql.py bin/filter /home/vbajpai/traces/ft/ july/queries/nfql/filter/
+sudo python nfql-filter.py "../bin/filter --verbose=1" /home/nmelnikov/traces/ft/ july/queries/nfql/filter/
