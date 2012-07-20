2d2d582f1cf6b7afdbf69bd638c87210c846fb15
Nikolay Melnikov <nmelnikov@crystal.eecs.jacobs-university.de>
2012-07-20 12:40:31 +0200

    updated run.sh with new paths

--- engine/benchmarks/july/run.sh
+++ engine/benchmarks/july/run.sh
@@ -1,14 +1,14 @@
 # run nfdump
-# sudo python nfdump.py /home/nmelnikov/traces/nf/ july/queries/nfdump/filter/
+sudo python nfdump.py /home/nmelnikov/traces/nf/ queries/nfdump/filter/
 
 # run flowtools
-# sudo python flowtools.py /home/nmelnikov/traces/ft/ july/queries/flowtools/filter/
+sudo python flowtools.py /home/nmelnikov/traces/ft/ queries/flowtools/filter/
 
 # run silk
-# sudo python silk.py /home/nmelnikov/traces/silk/ july/queries/silk/filter/
+sudo python silk.py /home/nmelnikov/traces/silk/ queries/silk/filter/
 
 # run nfql-engine
-# sudo python nfql-engine.py ../bin/engine /home/nmelnikov/traces/ft/ july/queries/nfql/filter/
+sudo python nfql-engine.py ../bin/engine /home/nmelnikov/traces/ft/ queries/nfql/filter/
 
 # run nfql-filter
-sudo python nfql-filter.py "../bin/filter --verbose=1" /home/nmelnikov/traces/ft/ july/queries/nfql/filter/
+sudo python nfql-filter.py "../bin/filter --verbose=1" /home/nmelnikov/traces/ft/ queries/nfql/filter/
