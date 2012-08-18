8bc1249035ce608822fadeb1a5d1c94aa6f70cb4
Vaibhav Bajpai <contact@vaibhavbajpai.com>
2012-08-18 11:17:35 +0200

    silk results for the merger stage

---
+++ engine/benchmarks/august/merger/run.sh
@@ -0,0 +1,5 @@
+# run silk
+sudo python silk.py /home/vbajpai/traces/silk/ queries/silk/
+
+# run nfql-engine
+# sudo python nfql-engine.py "/home/vbajpai/nfql/engine/bin/engine --verbose=1" /home/vbajpai/traces/ft/ queries/nfql/
