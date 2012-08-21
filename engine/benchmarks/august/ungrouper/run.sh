a1c201d21883cc3cd803f83f0a7382b10a4eb1db
Vaibhav Bajpai <contact@vaibhavbajpai.com>
2012-08-21 19:33:34 +0200

    added ungrouper results

--- engine/benchmarks/august/ungrouper/run.sh
+++ engine/benchmarks/august/ungrouper/run.sh
@@ -1,6 +1,4 @@
 # run nfql-engine
-#sudo python nfql-engine.py "/home/vbajpai/merger/engine/bin/engine" \
-#                          /home/vbajpai/traces/ft/ queries/nfql/
-
 sudo python nfql-engine.py "/home/vbajpai/merger/engine/bin/engine" \
-                           /tmp/traces/ queries/nfql/
+                           /home/vbajpai/traces/ft/ queries/nfql/
+
